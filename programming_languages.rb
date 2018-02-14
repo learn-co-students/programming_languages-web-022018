5d7e4b2d24dcbeec3603e6433e6693e0447f0ec6
lidianvilazio <lidianvilazio@hotmail.com>
2018-02-13 23:07:18 -0500

    Bonus - Programming Languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+    # your code here
+    new_hash = Hash.new(0)
+    languages.each do |k, v|
+        v.each do |k2, v2|
+            if new_hash.has_key?(k2)
+                new_hash[k2][:style].push(k)
+                else
+                new_hash[k2] = v2
+                new_hash[k2][:style] = []
+                new_hash[k2][:style].push(k)
+            end
+        end
+    end
+    new_hash
 end
+
