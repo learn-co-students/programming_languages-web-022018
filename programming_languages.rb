1a73cbf18789f8e7a92dba0549b8f70248e07cca
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-14 23:40:08 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = Hash.new
+  languages.each do |ooorf, languagesandtypes|
+    languagesandtypes.each do |language, type|
+        if new_hash.include?(language)
+          new_hash[language][:style].push(ooorf)
+        elsif
+          new_hash[language] = type
+          type[:style] = [ooorf]
+        end
+    end
+  end 
+  new_hash
 end
